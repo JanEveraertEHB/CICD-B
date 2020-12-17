b76c7a69fc6157ae6f8e45e48af31d6180f976f1
JanEveraertEHB <72449780+JanEveraertEHB@users.noreply.github.com>
2020-12-17 13:16:20 +0100

    adding cert

--- api/src/helper/DatabaseHelper.js
+++ api/src/helper/DatabaseHelper.js
@@ -1,31 +1,26 @@
-
+let fs = require('fs')
 let db = require('knex')({
   client: 'pg',
-  // connection: {
-  // host: process.env.POSTGRES_HOST || '127.0.0.1',
-  // user: process.env.POSTGRES_USER || 'example',
-  // password: process.env.POSTGRES_PASSWORD || 'example',
-  // database: process.env.POSTGRES_DB || 'test'
+  connection: {
+    user: "doadmin",
+    password: "qyouu9w2md8wud3b",
+    host: "test-students-do-user-2476832-0.b.db.ondigitalocean.com",
+    port: 25060,
+    database: "defaultdb",
+    charset: 'utf8',
+    ssl: {
+      ca: fs.readFileSync(__dirname + '/certs/ca-certificate.crt', 'ascii'),
+    },
 
-  // user: "doadmin",
-  // password: "qyouu9w2md8wud3b",
-  // host: "test-students-do-user-2476832-0.b.db.ondigitalocean.com",
-  // port: 25060,
-  // database: "defaultdb",
-  // sslmode: "require"
+  },
 
-  // },
-
-  connection: "postgresql://doadmin:show-password@private-test-students-do-user-2476832-0.b.db.ondigitalocean.com:25060/defaultdb?sslmode=require",
   pool: {
-    min: 0,
-    max: 7,
-    createTimeoutMillis: 3000,
-    acquireTimeoutMillis: 30000,
-    idleTimeoutMillis: 30000,
-    reapIntervalMillis: 1000,
-    createRetryIntervalMillis: 100,
-    propagateCreateError: false // <- default is true, set to false 
+    min: 2,
+    max: 50,
+    "idleTimeoutMillis": 30000,
+    "createTimeoutMillis": 30000,
+    "acquireTimeoutMillis": 30000,
+    propagateCreateError: true
   }
 });
 
